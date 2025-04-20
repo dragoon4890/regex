4801bac43c2ce652a9b3881021816c71a0d6f4d6
dragoon4890 <harshsks123@gmail.com>
2025-04-20 10:16:48 +0000

    added postfix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,36 @@ import { useState } from "react"
 function pp(e){
   pp =document.getElementById("rgx")
 
-  console.log( pp.value )
+  document.getElementById('postfix').innerText =pp.value
+}
+
+function con(){
+  pp =document.getElementById("rgx").value
+  // for now just  + - *
+  const precendence={'+':1,'-':1,'*':2,'/':2}
+  var operands= []
+  var ans=[]
+  for ( let i = 0 ; i < pp.length ; i++){
+    if (pp[i] in precendence){
+
+        while (operands.length && precendence[pp[i]] <=precendence[operands[operands.length -1]] ){
+          ans.push(operands.pop())
+        }
+        operands.push(pp[i])
+    }
+    else{
+      ans.push(pp[i])
+    }
+  }
+
+  while (operands.length){
+    ans.push(operands.pop())
+  }
+  var s=""
+  ans.forEach(char => {
+    s+=char
+  });
+  document.getElementById('postfix').innerText = s
 }
 
 
@@ -18,6 +47,8 @@ function App() {
 
     <input id="rgx" name ="Regex" placeholder="Regex" onChange={pp}></input>
     <label for="Regex">Enter the regex pls</label>
+    <p id="postfix"></p>
+    <button onClick={con}>Convert to Postfix</button>
     </>
   )
 }
